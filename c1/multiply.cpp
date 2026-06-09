0c5ebb9b3faa0c0e0261cf59953b0403154f2c87
Preston <prestontakacs@gmail.com>
2026-06-09 11:52:17 -0500

    basically done with c1

---
+++ c1/multiply.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{   
+    int num{};
+
+    std::cout << "Enter an integer: ";
+    std::cin >> num;
+    std::cout << "Double that number is: " << num * 2 << "\n";
+
+    return 0;
+}
