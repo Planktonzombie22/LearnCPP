f22bc7e21480cf72eef4fa711997ca2c83ae1032
Preston <prestontakacs@gmail.com>
2026-06-10 10:44:36 -0500

    started on c2

--- c1/multiply.cpp
+++ c1/multiply.cpp
@@ -6,7 +6,8 @@ int main()
 
     std::cout << "Enter an integer: ";
     std::cin >> num;
-    std::cout << "Double that number is: " << num * 2 << "\n";
+    std::cout << "Double " << num << " is: " << num * 2 << "\n";
+    std::cout << "Triple " << num << " is: " << num * 3 << "\n";
 
     return 0;
 }
